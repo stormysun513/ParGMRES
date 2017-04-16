496c34b14f99fb4751c7e25dcf0aa104f41c8cea
Chih-Wei Chang <bert.cwchang@gmail.com>
2017-04-15 22:08:42 -0400

    Add matrix type.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "main.hpp"
 #include "utils.hpp"
 #include "loadmtx.hpp"
+#include "mtxvec.hpp"
 
 #include <iostream>
 #include <vector>
@@ -14,103 +15,24 @@
 
 using namespace std;
 
-Vector
-mvAx(const vector<vector<double>>& A, const Vector& x) {
-    Vector b(A.size());
-
-    for (size_t i = 0; i < A.size(); ++i) {
-        b.set(i, 0);
-        for (size_t j = 0; j < A[0].size(); ++j) {
-            b.set(i, b.get(i) + A[i][j] * x.get(j));
-        }
-    }
-    return b;
-}
-
-// Like mvAx, but special case for Z(:, 1:j) * y
-Vector
-mvZy(const vector<vector<double>>& Z, const Vector& y, size_t j) {
-    assert(j == y.size());
-    Vector ret(Z.size());
-
-    for (size_t i = 0; i < Z.size(); ++i) {
-        ret.set(i, 0);
-        for (size_t k = 0; k < j; ++k) {
-            ret.set(i, ret.get(i) + Z[i][k] * y.get(k));
-        }
-    }
-    return ret;
-}
-
-vector<vector<double>>
-generateMatrix(int m, int n) {
-    vector<vector<double>> matrix(m);
-
-    for (size_t i = 0; i < m; i++) {
-        matrix[i] = vector<double>(n);
-    }
-    return matrix;
-}
-
-vector<vector<double>>
+Matrix
 identityMatrix(size_t size) {
-    vector<vector<double>> matrix(size);
+    Matrix mat(size);
 
     for (size_t i = 0; i < size; i++) {
-        matrix[i] = vector<double>(size);
-        matrix[i][i] = 1.0;
+        mat.set(i, i, 1.0);
     }
 
-    return matrix;
+    return mat;
 }
+
 
 void
-setRow(vector<vector<double>>& mat, const Vector& vec, int row) {
-
-    size_t length = vec.size();
-
-    assert(mat.size() > row);
-    assert(mat[0].size() == length);
-
-    for (int i = 0; i < length; i++) {
-        mat[row][i] = vec.get(i);
-    }
-}
-
-void
-setCol(vector<vector<double>>& mat, const Vector& vec, size_t col_idx) {
-
-    size_t length = vec.size();
-
-    assert(mat[0].size() > col_idx);
-    assert(mat.size() == length);
-
-    for (size_t i = 0; i < length; i++) {
-        mat[i][col_idx] = vec.get(i);
-    }
-}
-
-Vector
-getCol(const vector<vector<double>>& mat, size_t col_idx) {
-    size_t dim = mat.size();
-
-    assert(mat[0].size() > col_idx);
-
-    Vector col(mat.size());
-
-    for (size_t i = 0; i < dim; i++) {
-        col.set(i, mat[i][col_idx]);
-    }
-
-    return col;
-}
-
-void
-printMatrix(vector<vector<double>>& mat,
+printMatrix(Matrix& mat,
             size_t row_start, size_t row_end,
             size_t col_start, size_t col_end) {
-    size_t m = mat.size();
-    size_t n = mat[0].size();
+    size_t m = mat.nRows();
+    size_t n = mat.nCols();
 
     assert(row_start >= 0);
     assert(col_start >= 0);
@@ -120,12 +42,12 @@ printMatrix(vector<vector<double>>& mat,
     char buf[1024];
     sprintf(buf, "");
 
-    for (int i = row_start; i < row_end; i++) {
-        for (int j = col_start; j < col_end; j++) {
+    for (size_t i = row_start; i < row_end; i++) {
+        for (size_t j = col_start; j < col_end; j++) {
             if (j == col_start)
                 sprintf(buf, "%s[", buf);
 
-            sprintf(buf, "%s%.4f", buf, mat[i][j]);
+            sprintf(buf, "%s%.4f", buf, mat.get(i, j));
 
             if (j == col_end-1)
                 sprintf(buf, "%s]", buf);
@@ -139,7 +61,7 @@ printMatrix(vector<vector<double>>& mat,
 }
 
 Vector
-leastSquare(vector<vector<double>>& H, size_t size, double beta) {
+leastSquare(Matrix& H, size_t size, double beta) {
     Eigen::MatrixXf A(size+1, size);
     Eigen::VectorXf b = Eigen::VectorXf::Zero(size+1);
     Eigen::VectorXf y_(size);
@@ -149,7 +71,7 @@ leastSquare(vector<vector<double>>& H, size_t size, double beta) {
 
     for (int i = 0; i < size+1; i++) {
         for (int j = 0; j < size; j++) {
-            A(i, j) = H[i][j];
+            A(i, j) = H.get(i, j);
         }
     }
 
@@ -164,11 +86,11 @@ leastSquare(vector<vector<double>>& H, size_t size, double beta) {
 }
 
 Vector
-gmres(const vector<vector<double>>& A,
+gmres(const Matrix& A,
       const Vector& b,
       size_t m, double tol, size_t maxit) {
 
-    size_t dim = A[0].size();
+    size_t dim = A.nCols();
     size_t nit = 0;
     Vector x0(dim);
 
@@ -185,36 +107,38 @@ gmres(const vector<vector<double>>& A,
 
     Vector x(dim);
     while (nit < maxit) {
-        auto H = generateMatrix(m+1, m);
-        auto Z = generateMatrix(dim, m);
-        auto V = generateMatrix(dim, m+1);
+        Matrix H = Matrix(m+1, m);
+        Matrix Z = Matrix(dim, m);
+        Matrix V = Matrix(dim, m+1);
 
-        // TODO: the GMRES algorithm
-        auto r0 = b.sub(mvAx(A, x0));
+        Vector r0 = b.sub(A.mul(x0));
         double beta = r0.norm2();
-        setCol(V, r0.mulS(1.0 / beta), 0);
+        V.setCol(0, r0.mulS(1.0 / beta));
 
-        // TODO: get krylov space
+        // Generate krylov subspace
         for(int j = 0; j < m; j++) {
-            setCol(Z, mvAx(Prcnd, getCol(V, j)), j);
-            auto w = mvAx(A, getCol(Z, j));
+            // Z[:, j] = P * V[:, j]
+            Z.setCol(j, Prcnd.mul(V.getCol(j)));
 
-            for (int i = 0; i < j; i++) {
-                auto v = getCol(V, i);
-                H[i][j] = w.dotV(v);
-                w.isub(v.mulS(H[i][j]));
+            // w = A * Z[:, j]
+            Vector w = A.mul(Z.getCol(j));
+
+            for (size_t i = 0; i < j; i++) {
+                Vector v = V.getCol(i);
+                H.set(i, j, w.dotV(v));
+                w.isub(v.mulS(H.get(i, j)));
             }
 
-            H[j+1][j] = w.norm2();
-            setCol(V, w.mulS(1.0 / H[j+1][j]), j+1);
+            H.set(j+1, j, w.norm2());
+            V.setCol(j+1, w.mulS(1.0 / H.get(j+1, j)));
 
             // printMatrix(H, 0, j+1, 0, j);
             // cout << "---" << endl;
 
-            auto y = leastSquare(H, j+1, beta);
-            x = x0.add(mvZy(Z, y, j+1));
+            Vector y = leastSquare(H, j+1, beta);
+            x = x0.add(Z.mulPartial(y, j+1));
 
-            auto res_norm = mvAx(A, x).sub(b).norm2();
+            double res_norm = A.mul(x).sub(b).norm2();
 
             if (res_norm < tol * b.norm2()) {
                 cout << "FGMRES converged to relative tolerance: "
@@ -223,9 +147,6 @@ gmres(const vector<vector<double>>& A,
                      << nit << endl;
                 return x;
             }
-
-            // cout << "#" << j <<
-            //     " Residual=" << res_norm << endl;
         }
 
         x0 = x;
@@ -240,7 +161,7 @@ int main(int argc, char *argv[])
 
     auto A = loadMTXFile("../data/cage4.mtx");
 
-    auto b = Vector(A.size());
+    auto b = Vector(A.nCols());
     b.set(0, 1.0);
 
     int m = 100;
---
+++ src/mtxvec.hpp
@@ -0,0 +1,208 @@
+#ifndef __MTXVEC_H__
+#define __MTXVEC_H__
+
+#include <vector>
+#include <cmath>
+#include <cassert>
+
+class Vector
+{
+private:
+    std::vector<double> data;
+public:
+    Vector(size_t size) {
+        data = std::vector<double>(size);
+    }
+
+    void set(size_t idx, double val) {
+        data[idx] = val;
+    }
+
+    double get(size_t idx) const {
+        return data[idx];
+    }
+
+    size_t size() const {
+        return data.size();
+    }
+
+    double norm2() const {
+        double res = .0f;
+
+        for (double num: data) {
+            res += num * num;
+        }
+        return sqrt(res);
+    }
+
+    double dotV(Vector other) const {
+        double sum = .0f;
+
+        assert(data.size() == other.size());
+
+        for (size_t i = 0; i < data.size(); i++) {
+            sum += data[i] * other.get(i);
+        }
+
+        return sum;
+    }
+
+    Vector add(Vector other) const {
+        assert(data.size() == other.size());
+
+        Vector res(data.size());
+
+        for (size_t i = 0; i < data.size(); i++) {
+            res.set(i, data[i] + other.get(i));
+        }
+
+        return res;
+    }
+
+    // same as add, but inplace
+    Vector iadd(Vector other) {
+        assert(data.size() == other.size());
+
+        for (size_t i = 0; i < data.size(); i++) {
+            data[i] += other.get(i);
+        }
+
+        return *this;
+    }
+
+    Vector sub(Vector other) const {
+        assert(data.size() == other.size());
+
+        Vector res(data.size());
+
+        for (size_t i = 0; i < data.size(); i++) {
+            res.set(i, data[i] - other.get(i));
+        }
+
+        return res;
+    }
+
+    // same as sub, but inplace
+    Vector isub(Vector other) {
+        assert(data.size() == other.size());
+
+        for (size_t i = 0; i < data.size(); i++) {
+            data[i] -= other.get(i);
+        }
+
+        return *this;
+    }
+
+    Vector mulS(double scaler) const {
+        Vector res(data.size());
+
+        for (size_t i = 0; i < data.size(); i++) {
+            res.set(i, data[i] * scaler);
+        }
+
+        return res;
+    }
+
+    // same as mulS, but inplace
+    Vector imulS(double scaler) {
+        for (size_t i = 0; i < data.size(); i++) {
+            data[i] *= scaler;
+        }
+
+        return *this;
+    }
+
+    ~Vector() {};
+};
+
+class Matrix
+{
+private:
+    std::vector<vector<double>> data;
+    size_t n_rows;
+    size_t n_cols;
+public:
+    Matrix(size_t m, size_t n) {
+        data = std::vector<double>(m);
+        n_rows = m;
+        n_cols = n;
+        for (int i = 0; i < m; i++)
+            data[i] = std::vector<double>(n);
+    }
+
+    size_t nRows() const { return n_rows; }
+    size_t nCols() const { return n_cols; }
+
+    void set(size_t row_idx, size_t col_idx, double val) {
+        data[row_idx][col_idx] = val;
+    }
+
+    double get(size_t row_idx, size_t col_idx) const {
+        return data[row_idx][col_idx];
+    }
+
+    void setRow(size_t row_idx, const Vector& vec) {
+        size_t len = vec.size();
+
+        assert(0 <= row_idx);
+        assert(row_idx < n_rows);
+        assert(len == n_cols);
+
+        for (size_t i = 0; i < len; ++i) {
+            data[row_idx][i] = vec.get(i);
+        }
+    }
+
+    void setCol(size_t col_idx, const Vector& vec) {
+        size_t len = vec.size();
+
+        assert(0 <= col_idx);
+        assert(col_idx < n_cols);
+        assert(len == n_rows);
+
+        for (size_t i = 0; i < len; ++i) {
+            data[i][row_idx] = vec.get(i);
+        }
+    }
+
+    Vector getCol(size_t col_idx) const {
+        Vector col(n_rows);
+
+        for (int i = 0; i < n_rows; ++i) {
+            col.set(i, data[i][col_idx]);
+        }
+
+        return col;
+    }
+
+    Vector mul(const Vector& vec) const {
+        Vector ret(n_rows);
+
+        for (size_t i = 0; i < n_rows; ++i) {
+            ret.set(i, 0.0);
+            for (size_t j = 0; j < n_cols; ++j) {
+                ret.set(i, ret.get(i) + data[i][j] * vec.get(j));
+            }
+        }
+
+        return ret;
+    }
+
+    Vector mulPartial(const Vector& vec, size_t n_cols_) const {
+        assert(n_cols == vec.size());
+        Vector ret(n_rows);
+
+        for (size_t i = 0; i < n_rows; ++i) {
+            ret.set(i, 0.0);
+            for (size_t j = 0; j < n_cols_; ++j) {
+                ret.set(i, ret.get(i) + data[i][j] * vec.get(j));
+            }
+        }
+
+        return ret;
+    }
+
+    ~Matrix() {};
+};
+
+#endif  // MTXVEC_H
