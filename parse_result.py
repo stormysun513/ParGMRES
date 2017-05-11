9bd3be52fd9bb7d3bec0287abba761209b4c0fa1
Chih-Wei Chang <bert.cwchang@gmail.com>
2017-05-10 20:35:44 -0400

    Add results

---
+++ parse_result.py
@@ -0,0 +1,26 @@
+import numpy as np
+import re
+import sys
+
+log_file = sys.argv[1]
+
+with open(log_file, "r") as f:
+    lines = f.readlines()
+
+    nums = [[], [], [], []]
+    i = 0
+    while i < len(lines):
+        if lines[i][:6] == "FGMRES":
+            for j in range(1, 5):
+                line = lines[i+j]
+                nums[j-1].append(float(re.findall("\d+\.\d+", line)[0]))
+
+            i += 5
+        else:
+            i += 1
+
+    # print("Krylov =", nums[0])
+    print("Krylov mean =", np.mean(nums[0]), "std =", np.std(nums[0]))
+    print("LLS mean =", np.mean(nums[1]), "std =", np.std(nums[1]))
+    print("Calculate Residual mean =", np.mean(nums[2]), "std =", np.std(nums[2]))
+    print("Total mean =", np.mean(nums[3]), "std =", np.std(nums[3]))
--- src/run.sh
+++ src/run.sh
@@ -1,16 +1,47 @@
 #!/bin/bash
 
+rm logs/*
 
-for line in $(ls ../data/*.mtx); do
+for line in cage4.mtx cage5.mtx cage7.mtx cage8.mtx cage9.mtx cage10.mtx; do
     echo "===Matrix: $line==="
-    ./pgmres $line
-    printf "\n\n"
+
+    for i in {1..10}; do
+        ./dense_seq/dense_seq ../data/$line 2>&1 | tee -a logs/dense_seq_${line%.mtx}.log
+    done
+
+    printf "\n"
+
+    for i in {1..10}; do
+        ./dense_omp/dense_omp ../data/$line 2>&1 | tee -a logs/dense_omp_${line%.mtx}.log
+    done
+
+    printf "\n"
+
+    for i in {1..10}; do
+        ./sparse_omp/sparse_omp ../data/$line 2>&1 | tee -a logs/sparse_omp_${line%.mtx}.log
+    done
+
+    printf "\n"
 done
 
-# ./pgmres ../data/bcspwr01.mtx
-# ./pgmres ../data/bcspwr03.mtx
-# ./pgmres ../data/bcspwr06.mtx
-# ./pgmres ../data/cage4.mtx
-# ./pgmres ../data/cryg2500.mtx
-# ./pgmres ../data/mhd4800a.mtx
-# ./pgmres ../data/cryg10000.mtx
+for line in bcspwr01.mtx  bcspwr03.mtx  bcspwr06.mtx  bcspwr10.mtx; do
+    echo "===Matrix: $line==="
+
+    for i in {1..10}; do
+        ./dense_seq/dense_seq ../data/bcspwr/$line 2>&1 | tee -a logs/dense_seq_${line%.mtx}.log
+    done
+
+    printf "\n"
+
+    for i in {1..10}; do
+        ./dense_omp/dense_omp ../data/bcspwr/$line 2>&1 | tee -a logs/dense_omp_${line%.mtx}.log
+    done
+
+    printf "\n"
+
+    for i in {1..10}; do
+        ./sparse_omp/sparse_omp ../data/bcspwr/$line 2>&1 | tee -a logs/sparse_omp_${line%.mtx}.log
+    done
+
+    printf "\n"
+done
